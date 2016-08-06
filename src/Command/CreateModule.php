1a28ee1c665e67a70bdfdbd4faa33b097c8ec98b
Turcsan Adam <junior@legow.hu>
2016-08-06 18:36:47 +0200

    CreateModule can create module files: Module.php, module.config.php and a Controller example

--- src/Command/CreateModule.php
+++ src/Command/CreateModule.php
@@ -22,6 +22,45 @@ class CreateModule implements CommandInterface
     
     private $errorInfo = [];
     
+    private $moduleClassTemplate = '<?php 
+
+namespace %s;
+
+class Module {
+    public function getConfig()
+    {
+        return include __DIR__.\'/../config/module.config.php\';
+    }
+}
+';
+    private $indexControllerClassTemplate = '<?php 
+
+namespace %s\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+class IndexController extends AbstractActionController
+{
+    public function indexAction()
+    {
+        return new ViewModel();
+    }
+}
+';
+    private $moduleConfigTempalte = '<?php 
+
+namespace %s;
+
+return [
+    \'controllers\' => [
+        \'factories\' => [
+            Controller\IndexController::class => InvokableFactory::class,
+        ],
+    ]
+];
+';
+    
     public function feed($argument)
     {
         foreach($this->availableOptions as $name => $isRequired) {
@@ -46,10 +85,10 @@ class CreateModule implements CommandInterface
 
     public function run()
     {
-        if(class_exists('\\Zend\\ModuleManager\\ModuleManager')) {
-            echo "Fasza";
+        if(!class_exists('\\Zend\\ModuleManager\\ModuleManager')) {
+            throw new \Exception("Not in a Zend Framework project");
         }
-        throw new \Exception("Not in a Zend Framework project");
+        return $this->createModule($this->options["name"]);
     }
 
     public function errorInfo()
@@ -59,6 +98,25 @@ class CreateModule implements CommandInterface
         }
         return 1;
     }
-
-//put your code here
+    
+    public function createModule($name)
+    {
+        chdir('../');
+        if(basename(getcwd()) == 'vendor' || is_dir('../module')) {
+            //GOOD
+        } elseif (basename(getcwd()) == 'zf-tools') { //for develop pruposes
+            if(!is_dir('module')) {
+                mkdir('module/'.$name,0776, true);
+            }
+            chdir('module/'.$name);
+            mkdir('config', 0776);
+            mkdir('src/Controller', 0776, true);
+            $classDefinition = sprintf($this->moduleClassTemplate, $name);
+            file_put_contents('src/Module.php', $classDefinition);
+            $controllerDefinition = sprintf($this->indexControllerClassTemplate, $name);
+            file_put_contents('src/Controller/IndexController.php', $controllerDefinition);
+            $confgiDefinition = sprintf($this->moduleConfigTempalte, $name);
+            file_put_contents('config/module.config.php', $confgiDefinition);
+        }
+    }
 }
